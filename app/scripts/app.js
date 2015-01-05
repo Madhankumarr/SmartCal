008df1eab553837d2c932a295329936730b09ed0
Madhankumar <madhankumarr@hotmail.com>
2015-01-05 17:46:06 +0530

    init

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name smartcalendarApp
+ * @description
+ * # smartcalendarApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('smartcalendarApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name smartcalendarApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the smartcalendarApp
+ */
+angular.module('smartcalendarApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
